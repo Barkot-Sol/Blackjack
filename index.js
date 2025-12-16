b60e22b7e0158820c9d5d567cab2cf53c5767beb
Barkot Solomon <barkotsolomon5@gmail.com>
2025-12-16 19:31:46 +0300

    Add player info and new card functionality to game
    Introduced a player object with name and chips, added display for player info, and implemented the ability to draw a new card with a dedicated button. Refactored card handling logic.

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
-let firstCard = 10
-let secondCard = 4
-let sum = firstCard + secondCard
+let player = {
+    name: "Barkot",
+    chips: 200
+}
+
+let cards = []
+let sum = 0
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.querySelector(".sum-el")
+let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name + ": $" + player.chips
+
+function getRandomCard() {
+    let randomNumer = Math.floor( Math.random()*13 ) + 1
+    if (randomNumer > 10) {
+        return 10
+    } else if (randomNumer === 1) {
+        return 11
+    } else {
+        return randomNumer
+    }
+}
 
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: ";
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -21,3 +54,12 @@ function startGame() {
     messageEl.textContent = message
     console.log(message)    
 }
+function newCard() {
+    if(isAlive == true && hasBlackJack == false){
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()
+    }
+}
+    
